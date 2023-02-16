3888d9af267cb6f8488445b9781d0caf50ad23c2
TiagoFP <tiagopimenteldev@gmail.com>
2023-02-16 03:30:36 +0000

    Arquivos projeto 1 do Linux

---
+++ criar_user.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Criando usuários do sistema"
+
+useradd guest10 -c "Usuário convidado" -m -s/bin/bash
+
+useradd guest11 -c "Usuário convidado" -m -s/bin/bash
+
+useradd guest12 -c "Usuário convidado" -m -s/bin/bash
+
+useradd guest13 -c "Usuário convidado" -m -s/bin/bash
+
+echo "Finalizado"
+
